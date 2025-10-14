3f3c281394b6eb18f4f2b63157d7cfab2ca04008
LękTomaszaHajto <147493791+Bartuster@users.noreply.github.com>
2025-10-14 09:02:41 +0200

    Update PageOne.jsx

--- src/pages/PageOne.jsx
+++ src/pages/PageOne.jsx
@@ -4,7 +4,7 @@ export default function PageOne() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <h1 className="text-3xl font-bold mb-4 text-black">Page One</h1>
-      <p className="mb-4 text-black">To jest random tekst na Page One</p>
+      <p className="mb-4 text-black">Marek Marucha ciągnie druta</p>
       <Link to="/pagetwo" className="px-4 py-2 bg-black text-white rounded hover:bg-gray-800">
         Przejdź do Page Two
       </Link>
